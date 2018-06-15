c5abad089681dcd0ed2e8daa0f96f8a1643c269b
NomerSekawan <syarifhidayatullah.it@gmail.com>
2018-06-16 00:21:18 +0700

    0.1

---
+++ css-file.php
@@ -0,0 +1,11 @@
+<!-- Bootstrap CSS File -->
+<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="lib/nivo-slider/css/nivo-slider.css" rel="stylesheet">
+<link href="lib/owlcarousel/owl.carousel.css" rel="stylesheet">
+<link href="lib/owlcarousel/owl.transitions.css" rel="stylesheet">
+<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="lib/animate/animate.min.css" rel="stylesheet">
+<link href="lib/venobox/venobox.css" rel="stylesheet">
+<link href="css/nivo-slider-theme.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
---
+++ index.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home - OldFace</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+  <?php include 'css-file.php' ?>
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example">
+  <div id="preloader"></div>
+  <header>
+    <div id="sticker" class="header-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+            <nav class="navbar navbar-default">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                <a class="navbar-brand page-scroll sticky-logo" href="index.php">
+                  <h1><span>Old</span>Face</h1>
+                  <!-- Uncomment below if you prefer to use an image logo -->
+                  <!-- <img src="img/logo.png" alt="" title=""> -->
+                </a>
+              </div>
+              <div class="collapse navbar-collapse main-menu bs-example-navbar-collapse-1" id="navbar-example">
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="active">
+                    <a class="page-scroll" href="index.php">Home</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="#" href="#about">OldFace</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="#">Kaos OldFace</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="#">Bahan Kaos</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="#">Pesan Kaos</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div id="home" class="slider-area">
+    <div class="bend niceties preview-2">
+      <div id="ensign-nivoslider" class="slides">
+        <img src="img/slider/slider-1.jpg" alt="" title="#slider-direction-1" />
+        <img src="img/slider/slider-2.jpg" alt="" title="#slider-direction-2" />
+        <img src="img/slider/slider-3.jpg" alt="" title="#slider-direction-3" />
+        <img src="img/slider/slider-4.jpg" alt="" title="#slider-direction-4" />
+      </div>
+
+      <div id="slider-direction-1" class="slider-direction slider-one">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">Slider 1 </h2>
+                </div>
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <!-- <h1 class="title2">Isi keterangan slide</h1> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slider-direction-2" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content text-center">
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">Slider 2 </h2>
+                </div>
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <!-- <h1 class="title2">Isi keterangan slide</h1> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slider-direction-3" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">Slider 3 </h2>
+                </div>
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <!-- <h1 class="title2">Isi keterangan slide</h1> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="slider-direction-4" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">Slider 4 </h2>
+                </div>
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <!-- <h1 class="title2">Isi keterangan slide</h1> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include 'footer.php' ?>
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <?php include 'js-file.php' ?>
+</body>
+</html>
